6b28e90cf76aea53e6fe14f63374491c80816456
opekope2 <opekope2@gmail.com>
2023-01-11 22:14:39 +0100

    Remove code as it is irrelevant

--- OptiGUI/kotlin/opekope2/optigui/internal/TextureReplacer.kt
+++
@@ -1,9 +0,0 @@
-package opekope2.optigui.internal
-
-import opekope2.optigui.wrapper.Identifier
-
-object TextureReplacer {
-    fun getReplacement(texture: Identifier): Identifier {
-        return texture // TODO
-    }
-}
